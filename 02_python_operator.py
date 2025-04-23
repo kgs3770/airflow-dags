8ce3c2516ad1ba85cb2a82a16a3b9af274257c1b
kgs3770 <kgs3770@gmail.com>
2025-04-23 11:26:38 +0900

    update

---
+++ 02_python_operator.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+def hello():
+    print('hello world')
+
+def bye():
+    print('bye...')
+
+with DAG(
+    dag_id='02_python',
+    description='python test',
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    schedule=timedelta(minutes=1)
+) as dag:
+    t1 = PythonOperator(
+        task_id='hello',
+        python_callable=hello
+    )
+
+    t2 = PythonOperator(
+        task_id='bye',
+        python_callable=bye
+    )
+
+    t1 >> t2
